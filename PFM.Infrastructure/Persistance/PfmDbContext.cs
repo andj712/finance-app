5769476b35019de5c9aad684d3b819d4a6c2ce3a
andj712 <andj712@gmail.com>
2025-07-20 01:08:18 +0200

    feat/init:setting up connection to database

---
+++ PFM.Infrastructure/Persistance/PfmDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PFM.Infrastructure.Persistance
+{
+    public class PfmDbContext : DbContext
+    {
+
+        public PfmDbContext(DbContextOptions<PfmDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        
+    }
+}
--- finance-app/Program.cs
+++ finance-app/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using PFM.Infrastructure.Persistance;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+DotNetEnv.Env.Load();
+var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+if (string.IsNullOrWhiteSpace(connectionString))
+{
+    throw new InvalidOperationException("Connection string not found in environment variables.");
+}
+builder.Services.AddDbContext<PfmDbContext>(options =>
+    options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
