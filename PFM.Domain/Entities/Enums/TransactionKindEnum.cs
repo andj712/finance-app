d8bef3e1289d13019efe95fcecd80ddc7dbd9acc
andj712 <andj712@gmail.com>
2025-07-20 00:44:50 +0200

    feat/init: add entities

---
+++ PFM.Domain/Entities/Enums/TransactionKindEnum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PFM.Domain.Entities.Enums
+{
+    public enum TransactionKindEnum
+    {
+        dep,        // Deposit  
+        wdw,        // Withdrawal  
+        pmt,        // Payment  
+        fee,        // Fee  
+        inc,        // Interest  
+        rev,        // Reversal  
+        adj,        // Adjustment  
+        lnd,        // LoanDisbursement  
+        lnr,        // LoanRepayment  
+        fcx,        // ForeignExchange  
+        aop,        // AccountOpening  
+        acl,        // AccountClosing  
+        spl,        // SplitPayment  
+        sal         // Salary  
+    }
+}
---
+++ PFM.Domain/Entities/Transaction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PFM.Domain.Entities
+{
+    internal class Transaction
+    {
+        [Key]
+        public string Id { get; set; } = string.Empty;
+
+        public string BeneficiaryName { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public DirectionEnum Direction { get; set; }  
+
+        [Required]
+        public decimal Amount { get; set; }
+
+
+        public string? Description { get; set; }
+
+        [Required]
+        public string Currency { get; set; } = "USD";
+
+        public MccCodeEnum? MccCode { get; set; }
+
+        public string Kind { get; set; }
+    }
+}
